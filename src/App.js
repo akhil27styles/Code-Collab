2f2f838251f6fec76a08c4a3eb86499ccea9022a
Akhil kumar singh <akhil2771999@gmail.com>
2023-07-24 22:31:19 +0530

    toast and naviagate link added from home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { BrowserRouter ,Routes, Route} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
 import Home from './pages/Home/Home'
 import EditorPage from './pages/EditorPage'
 import './App.css'
 const App = () => {
   return (
     <>
+    <div>
+      <Toaster
+      position='top-right'
+      toastOptions={{
+        success:{
+          theme:{
+            primary:'#4aed8B'
+          }
+        }
+      }}
+      ></Toaster>
+    </div>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
