30f9b05c1958592d2405c9d2248fca39334f607e
Akhil Kumar Singh <52193850+akhil27styles@users.noreply.github.com>
2023-07-30 23:31:36 +0530

    Feature editor page (#9)
    * added uuid for input
    
    * toast and naviagate link added from home page
    
    * edtorPage setup
    
    * editor design (#5)
    
    * Adding Dark Theme to IDE.
    
    * Update Ide.js
    
    * feature-ediorPage-refractor
    
    ---------
    
    Co-authored-by: poynting-dev <dixitpriyanshu23@gmail.com>
    Co-authored-by: Priyanshu Dixit <74833156+poynting-dev@users.noreply.github.com>

--- src/components/Ide.js
+++ src/components/Ide.js
@@ -1,55 +1,30 @@
-import React, { useState } from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import 'react-codemirror/node_modules/codemirror/mode/javascript/javascript';
-import 'react-codemirror/node_modules/codemirror/mode/css/css';
-import 'react-codemirror/node_modules/codemirror/addon/hint/show-hint';
-import 'react-codemirror/node_modules/codemirror/addon/hint/javascript-hint';
-import 'react-codemirror/node_modules/codemirror/theme/monokai.css';
-const languageModes = [
-  { name: 'javascript', json: true },
-  { name: 'python', version: 3 },
-  { name: 'ruby' },
-];
+import React, { useEffect, useRef } from 'react';
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/edit/closebrackets';
 
 const Ide = () => {
-  const [code, setCode] = useState('//:code');
-  const [selectedLanguage, setSelectedLanguage] = useState('javascript');
+  const editorRef = useRef(null);
+     useEffect(() => {
+        async function init() {
+            editorRef.current = Codemirror.fromTextArea(
+                document.getElementById('realtimeEditor'),
+                {
+                    mode: { name: 'javascript', json: true },
+                    theme: 'dracula',
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    lineNumbers: true,
+                }
+            );
+        }
+        init();
+    }, []);
 
-  const handleLanguageChange = (event) => {
-    setSelectedLanguage(event.target.value);
-  };
-
-  const updateCode = (newCode) => {
-    setCode(newCode);
-  };
-
-  return (
-    <div>
-      <div>
-        <label htmlFor="language">Select Language:</label>
-        <select id="language" value={selectedLanguage} onChange={handleLanguageChange}>
-          {languageModes.map((mode) => (
-            <option key={mode.name} value={mode.name}>
-              {mode.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <CodeMirror
-        onChange={() => updateCode(code)}
-        value={code}
-        options={{
-          theme: 'monokai',
-          keyMap: 'sublime',
-          mode: selectedLanguage, 
-          lineNumbers: true,
-          extraKeys: {
-            'Ctrl-Space': 'autocomplete',
-          },
-        }}
-      />
-    </div>
-  );
+    return <textarea id="realtimeEditor"></textarea>;
 };
 
 export default Ide;
