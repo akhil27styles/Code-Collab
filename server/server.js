b82702250512d13d64b24f92e7be76f4d24ada3b
Akhil Kumar Singh <52193850+akhil27styles@users.noreply.github.com>
2023-07-31 00:04:05 +0530

    Feature server (#10)
    * feature-server
    
    * .env ignore
    
    * username check and leaveRoom fun (#8)

---
+++ server/server.js
@@ -0,0 +1,61 @@
+const express=require('express');
+const app=express();
+const http=require('http');
+const {Server}=require('socket.io');
+const ACTIONS = require('../src/constants/Actions');
+
+const server=http.createServer(app);
+const io=new Server(server);
+
+const userSocketMap={};
+
+function getAllConnectedClients(roomId){
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId)=>{
+       return {
+        socketId,
+        username:userSocketMap[socketId],
+       };
+     }
+    );
+}
+io.on('connection',(socket)=>{
+    console.log(`socket connected`,socket.id);
+
+    socket.on(ACTIONS.JOIN,({roomId,username})=>{
+
+      // check to Don't add same username again 
+      const existingSocketId = Object.keys(userSocketMap).find(
+      (socketId) => userSocketMap[socketId] === username);
+      if(existingSocketId){
+        return;
+       }
+
+     userSocketMap[socket.id]=username;
+     socket.join(roomId);
+     const clients=getAllConnectedClients(roomId);
+     
+     clients.forEach(({socketId})=>{
+        io.to(socketId).emit(ACTIONS.JOINED,{
+            clients,
+            username,
+            socketId:socket.id,
+        });
+     });
+ });
+
+  socket.on('disconnecting',()=>{
+    const rooms=[...socket.rooms];
+    rooms.forEach((roomId)=>{
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
+        socketId:socket.id,
+        username:userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  })
+});
+
+const PORT=process.env.PORT || 5000;
+server.listen(PORT,()=> console.log(`I am on ${PORT}`));
